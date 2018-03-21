851b731c5b6feda21e20ccd284c8dec1ba2746d9
TitoNadar <titonadar06@gmail.com>
2018-03-21 19:34:00 +0530

    networking retrofit done

---
+++ app/src/main/java/tito/example/com/cryptonews/Helper/Common.java
@@ -0,0 +1,11 @@
+package tito.example.com.cryptonews.Helper;
+
+/**
+ * Created by tito on 21/3/18.
+ */
+
+public class Common {
+    private static String Base_Url="https://newsapi.org/";
+    public static final String API_KEY="44aa4bd036d74cd9a38c0c5f485c3557";
+
+}
---
+++ app/src/main/java/tito/example/com/cryptonews/Interface/ItemClickListener.java
@@ -0,0 +1,11 @@
+package tito.example.com.cryptonews.Interface;
+
+import android.view.View;
+
+/**
+ * Created by tito on 21/3/18.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
